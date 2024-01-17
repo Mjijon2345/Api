72959df086ae53bf2f098a5a4f1a4410b9a84dde
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-01-17 13:29:41 -0500

    detalles codigo

--- Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -1,7 +1,14 @@
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
+    /// <summary>
+    /// Descripción de modelo para colecciones.
+    /// Extiende la clase base ModelDescription.
+    /// </summary>
     public class CollectionModelDescription : ModelDescription
     {
+        /// <summary>
+        /// Obtiene o establece la descripción del elemento en la colección.
+        /// </summary>
         public ModelDescription ElementDescription { get; set; }
     }
 }
--- Areas/HelpPage/ModelDescriptions/ModelNameAttribute.cs
+++ Areas/HelpPage/ModelDescriptions/ModelNameAttribute.cs
@@ -3,16 +3,23 @@ using System;
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
     /// <summary>
-    /// Use this attribute to change the name of the <see cref="ModelDescription"/> generated for a type.
+    /// Utilice este atributo para cambiar el nombre de la <see cref="ModelDescription"/> generada para un tipo.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
     public sealed class ModelNameAttribute : Attribute
     {
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase ModelNameAttribute con el nombre proporcionado.
+        /// </summary>
+        /// <param name="name">El nuevo nombre de la descripción del modelo.</param>
         public ModelNameAttribute(string name)
         {
             Name = name;
         }
 
+        /// <summary>
+        /// Obtiene el nombre de la descripción del modelo.
+        /// </summary>
         public string Name { get; private set; }
     }
 }
