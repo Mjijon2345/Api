c933e31f6b47031886c13fa2ec5af58cde20553a
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-01-17 13:34:33 -0500

    mas correciones

--- Areas/HelpPage/ModelDescriptions/ModelDescription.cs
+++ Areas/HelpPage/ModelDescriptions/ModelDescription.cs
@@ -3,14 +3,23 @@ using System;
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
     /// <summary>
-    /// Describes a type model.
+    /// Describe un modelo de tipo.
     /// </summary>
     public abstract class ModelDescription
     {
+        /// <summary>
+        /// Obtiene o establece la documentación asociada al modelo.
+        /// </summary>
         public string Documentation { get; set; }
 
+        /// <summary>
+        /// Obtiene o establece el tipo del modelo.
+        /// </summary>
         public Type ModelType { get; set; }
 
+        /// <summary>
+        /// Obtiene o establece el nombre del modelo.
+        /// </summary>
         public string Name { get; set; }
     }
 }
--- Areas/HelpPage/ModelDescriptions/ModelNameHelper.cs
+++ Areas/HelpPage/ModelDescriptions/ModelNameHelper.cs
@@ -5,28 +5,39 @@ using System.Reflection;
 
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
+    /// <summary>
+    /// Clase de ayuda interna para obtener nombres de modelo personalizados.
+    /// </summary>
     internal static class ModelNameHelper
     {
-        // Modify this to provide custom model name mapping.
+        // Modificar este método para proporcionar un mapeo personalizado de nombres de modelo.
         public static string GetModelName(Type type)
         {
+            // Obtiene el atributo ModelNameAttribute del tipo, si existe.
             ModelNameAttribute modelNameAttribute = type.GetCustomAttribute<ModelNameAttribute>();
             if (modelNameAttribute != null && !String.IsNullOrEmpty(modelNameAttribute.Name))
             {
                 return modelNameAttribute.Name;
             }
 
+            // Si no hay atributo, utiliza el nombre del tipo como nombre de modelo.
             string modelName = type.Name;
+
+            // Si el tipo es genérico, formatea el nombre del tipo genérico.
             if (type.IsGenericType)
             {
-                // Format the generic type name to something like: GenericOfAgurment1AndArgument2
+                // Obtiene el tipo genérico y sus argumentos.
                 Type genericType = type.GetGenericTypeDefinition();
                 Type[] genericArguments = type.GetGenericArguments();
                 string genericTypeName = genericType.Name;
 
-                // Trim the generic parameter counts from the name
+                // Elimina los conteos de parámetros genéricos del nombre.
                 genericTypeName = genericTypeName.Substring(0, genericTypeName.IndexOf('`'));
+
+                // Obtiene los nombres de los argumentos de tipo.
                 string[] argumentTypeNames = genericArguments.Select(t => GetModelName(t)).ToArray();
+
+                // Formatea el nombre del modelo para tipos genéricos.
                 modelName = String.Format(CultureInfo.InvariantCulture, "{0}Of{1}", genericTypeName, String.Join("And", argumentTypeNames));
             }
 
