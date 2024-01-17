58cdfdf4ac7133b1d636da27634bae36a5bf4015
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-01-17 13:38:22 -0500

    Ultimo commit

--- Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
+++ Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -2,9 +2,18 @@ using System;
 
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
+
+    /// Clase que representa una anotación de parámetro.
+
     public class ParameterAnnotation
     {
+
+        /// Obtiene o establece el atributo de anotación.
+
         public Attribute AnnotationAttribute { get; set; }
+
+
+        /// Obtiene o establece la documentación asociada a la anotación.
 
         public string Documentation { get; set; }
     }
--- Areas/HelpPage/ModelDescriptions/ParameterDescription.cs
+++ Areas/HelpPage/ModelDescriptions/ParameterDescription.cs
@@ -3,19 +3,28 @@ using System.Collections.ObjectModel;
 
 namespace Api.Areas.HelpPage.ModelDescriptions
 {
+
+    /// Clase que describe un parámetro.
+
     public class ParameterDescription
     {
+
+        /// Inicializa una nueva instancia de la clase ParameterDescription.
         public ParameterDescription()
         {
             Annotations = new Collection<ParameterAnnotation>();
         }
 
+        /// Obtiene las anotaciones asociadas al parámetro.
         public Collection<ParameterAnnotation> Annotations { get; private set; }
 
+        /// Obtiene o establece la documentación asociada al parámetro.
         public string Documentation { get; set; }
 
+        /// Obtiene o establece el nombre del parámetro.
         public string Name { get; set; }
 
+        /// Obtiene o establece la descripción del tipo de parámetro.
         public ModelDescription TypeDescription { get; set; }
     }
 }
