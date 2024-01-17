c60bfb7390c1a72dac53b468131ee20371e3e895
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-01-17 13:20:22 -0500

    comantarios clase Hotels

--- Areas/HelpPage/Controllers/HotelsController.cs
+++ Areas/HelpPage/Controllers/HotelsController.cs
@@ -17,14 +17,18 @@ namespace Api.Areas.HelpPage.Controllers
         {
             try
             {
+                // Inicializa una lista para almacenar objetos Hotel
                 List<Hotel> hotels = new List<Hotel>();
 
+                // Establece una conexión a la base de datos
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
 
+                    // Ejecuta una consulta SQL para recuperar todos los hoteles
                     using (SqlCommand cmd = new SqlCommand("SELECT * FROM hoteles", connection))
                     {
+                        // Lee datos desde SqlDataReader y popula objetos Hotel
                         using (SqlDataReader reader = cmd.ExecuteReader())
                         {
                             while (reader.Read())
@@ -37,136 +41,163 @@ namespace Api.Areas.HelpPage.Controllers
                                     Price = reader["Price"] is DBNull ? null : (int?)reader["Price"]
                                 };
 
+                                // Agrega el objeto Hotel poblado a la lista
                                 hotels.Add(hotel);
                             }
                         }
                     }
                 }
 
+                // Devuelve una respuesta con la lista de hoteles
                 return Request.CreateResponse(HttpStatusCode.OK, hotels);
             }
             catch (Exception ex)
             {
+                // Maneja las excepciones y devuelve una respuesta de error interno del servidor
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
             }
         }
 
         // GET: api/Hotels/5
         public IHttpActionResult Get(int id)
+        {
+            // Inicializa un objeto Hotel
+            Hotel hotel = null;
+
+            // Establece una conexión a la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                Hotel hotel = null;
+                connection.Open();
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                // Ejecuta una consulta SQL parametrizada para recuperar un hotel específico por Id
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM hoteles WHERE IdHotel = @IdHotel", connection))
                 {
-                    connection.Open();
+                    // Agrega un parámetro para el Id del hotel
+                    cmd.Parameters.AddWithValue("@IdHotel", id);
 
-                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM hoteles WHERE IdHotel = @IdHotel", connection))
+                    // Lee datos desde SqlDataReader y popula el objeto Hotel
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        cmd.Parameters.AddWithValue("@IdHotel", id);
-
-                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        if (reader.Read())
                         {
-                            if (reader.Read())
+                            hotel = new Hotel
                             {
-                                hotel = new Hotel
-                                {
-                                    IdHotel = (int)reader["IdHotel"],
-                                    TitleHotel = reader["TitleHotel"].ToString(),
-                                    DescriptionHotel = reader["DescriptionHotel"].ToString(),
-                                    Price = (decimal?)reader["Price"]
-                                };
-                            }
+                                IdHotel = (int)reader["IdHotel"],
+                                TitleHotel = reader["TitleHotel"].ToString(),
+                                DescriptionHotel = reader["DescriptionHotel"].ToString(),
+                                Price = (decimal?)reader["Price"]
+                            };
                         }
                     }
                 }
-
-                if (hotel == null)
-                {
-                    return NotFound();
-                }
-
-                return Ok(hotel);
             }
 
-            // POST: api/Hotels
-            public IHttpActionResult Post([FromBody] Hotel hotel)
+            // Verifica si se encontró el hotel y devuelve una respuesta apropiada
+            if (hotel == null)
             {
-                if (hotel == null)
-                {
-                    return BadRequest("El hotel no puede ser nulo.");
-                }
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    using (SqlCommand cmd = new SqlCommand("INSERT INTO hoteles (TitleHotel, DescriptionHotel, Price) VALUES (@TitleHotel, @DescriptionHotel, @Price); SELECT SCOPE_IDENTITY()", connection))
-                    {
-                        cmd.Parameters.AddWithValue("@TitleHotel", hotel.TitleHotel);
-                        cmd.Parameters.AddWithValue("@DescriptionHotel", hotel.DescriptionHotel);
-                        cmd.Parameters.AddWithValue("@Price", hotel.Price ?? (object)DBNull.Value);
-
-                        int newId = Convert.ToInt32(cmd.ExecuteScalar());
-                        hotel.IdHotel = newId;
-                    }
-                }
-
-                return CreatedAtRoute("DefaultApi", new { id = hotel.IdHotel }, hotel);
+                return NotFound();
             }
 
-            // PUT: api/Hotels/5
-            public IHttpActionResult Put(int id, [FromBody] Hotel hotel)
-            {
-                if (hotel == null)
-                {
-                    return BadRequest("El hotel no puede ser nulo.");
-                }
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    using (SqlCommand cmd = new SqlCommand("UPDATE hoteles SET TitleHotel = @TitleHotel, DescriptionHotel = @DescriptionHotel, Price = @Price WHERE IdHotel = @IdHotel", connection))
-                    {
-                        cmd.Parameters.AddWithValue("@IdHotel", id);
-                        cmd.Parameters.AddWithValue("@TitleHotel", hotel.TitleHotel);
-                        cmd.Parameters.AddWithValue("@DescriptionHotel", hotel.DescriptionHotel);
-                        cmd.Parameters.AddWithValue("@Price", hotel.Price ?? (object)DBNull.Value);
-
-                        int rowsAffected = cmd.ExecuteNonQuery();
-
-                        if (rowsAffected == 0)
-                        {
-                            return NotFound();
-                        }
-                    }
-                }
-
-                return Ok(hotel);
-            }
-
-            // DELETE: api/Hotels/5
-            public IHttpActionResult Delete(int id)
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    using (SqlCommand cmd = new SqlCommand("DELETE FROM hoteles WHERE IdHotel = @IdHotel", connection))
-                    {
-                        cmd.Parameters.AddWithValue("@IdHotel", id);
-
-                        int rowsAffected = cmd.ExecuteNonQuery();
-
-                        if (rowsAffected == 0)
-                        {
-                            return NotFound();
-                        }
-                    }
-                }
-
-                return Ok();
-            }
+            return Ok(hotel);
         }
- }
 
+        // POST: api/Hotels
+        public IHttpActionResult Post([FromBody] Hotel hotel)
+        {
+            // Verifica si el objeto hotel proporcionado es nulo
+            if (hotel == null)
+            {
+                return BadRequest("El hotel no puede ser nulo.");
+            }
+
+            // Establece una conexión a la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Ejecuta una consulta SQL para insertar un nuevo hotel y obtener su Id
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO hoteles (TitleHotel, DescriptionHotel, Price) VALUES (@TitleHotel, @DescriptionHotel, @Price); SELECT SCOPE_IDENTITY()", connection))
+                {
+                    // Agrega parámetros para las propiedades del hotel
+                    cmd.Parameters.AddWithValue("@TitleHotel", hotel.TitleHotel);
+                    cmd.Parameters.AddWithValue("@DescriptionHotel", hotel.DescriptionHotel);
+                    cmd.Parameters.AddWithValue("@Price", hotel.Price ?? (object)DBNull.Value);
+
+                    // Ejecuta la consulta y obtiene el nuevo Id
+                    int newId = Convert.ToInt32(cmd.ExecuteScalar());
+                    hotel.IdHotel = newId;
+                }
+            }
+
+            // Devuelve una respuesta con el hotel recién creado y su Id
+            return CreatedAtRoute("DefaultApi", new { id = hotel.IdHotel }, hotel);
+        }
+
+        // PUT: api/Hotels/5
+        public IHttpActionResult Put(int id, [FromBody] Hotel hotel)
+        {
+            // Verifica si el objeto hotel proporcionado es nulo
+            if (hotel == null)
+            {
+                return BadRequest("El hotel no puede ser nulo.");
+            }
+
+            // Establece una conexión a la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Ejecuta una consulta SQL parametrizada para actualizar un hotel existente por Id
+                using (SqlCommand cmd = new SqlCommand("UPDATE hoteles SET TitleHotel = @TitleHotel, DescriptionHotel = @DescriptionHotel, Price = @Price WHERE IdHotel = @IdHotel", connection))
+                {
+                    // Agrega parámetros para las propiedades del hotel y el Id
+                    cmd.Parameters.AddWithValue("@IdHotel", id);
+                    cmd.Parameters.AddWithValue("@TitleHotel", hotel.TitleHotel);
+                    cmd.Parameters.AddWithValue("@DescriptionHotel", hotel.DescriptionHotel);
+                    cmd.Parameters.AddWithValue("@Price", hotel.Price ?? (object)DBNull.Value);
+
+                    // Ejecuta la consulta y verifica el número de filas afectadas
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    // Si no se afectaron filas, el hotel no se encontró
+                    if (rowsAffected == 0)
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+
+            // Devuelve una respuesta con el hotel actualizado
+            return Ok(hotel);
+        }
+
+        // DELETE: api/Hotels/5
+        public IHttpActionResult Delete(int id)
+        {
+            // Establece una conexión a la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Ejecuta una consulta SQL parametrizada para eliminar un hotel por Id
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM hoteles WHERE IdHotel = @IdHotel", connection))
+                {
+                    // Agrega un parámetro para el Id del hotel
+                    cmd.Parameters.AddWithValue("@IdHotel", id);
+
+                    // Ejecuta la consulta y verifica el número de filas afectadas
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    // Si no se afectaron filas, el hotel no se encontró
+                    if (rowsAffected == 0)
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+
+            // Devuelve una respuesta indicando una eliminación exitosa
+            return Ok();
+        }
+    }
+}
